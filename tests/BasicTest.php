d1e8dd913fc13dd95d0e0126dc2210ea4a316ee2
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2019-07-22 17:47:08 +0200

    Add PHPUnit

---
+++ tests/BasicTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nmokkenstorm\LarvelQueueStatistics\Test;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_work()
+    {
+        $this->assertTrue(true);
+    }
+}
