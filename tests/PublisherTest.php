32eeeb3195cad1f91941b81a28018dbe42554299
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2019-07-23 11:41:15 +0200

    Make publish test a bit slimmer

--- tests/PublisherTest.php
+++ tests/PublisherTest.php
@@ -11,35 +11,42 @@ use Nmokkenstorm\LaravelQueueStatistics\Contracts\PublishesQueueStatistics;
 class PublisherTest extends TestCase
 {
     /**
-     * @test
+     * Hook into the testing famework 
      */
-    public function it_should_flush_correctly()
+    protected function setUp() : void
     {
-        $flushStrategy  = \Mockery::mock(FlushStrategy::class);
-        $publishBackend = \Mockery::mock(PublishesQueueStatistics::class);
+        parent::setUp();
 
-        $parameters = [
-            [
+        $this->flushStrategy  = \Mockery::mock(FlushStrategy::class);
+        $this->publishBackend = \Mockery::mock(PublishesQueueStatistics::class);
+
+        $this->parameters = [
                 'id'        => 'fake-id',
                 'event'     => 'fake-event',
                 'job'       => 'fake-job',
                 'queue'     => 'fake-queue',
                 'timestamp' => Carbon::now()->timestamp
-            ]
         ];
+    }
 
-        $flushStrategy->shouldReceive('shouldFlush')
-                      ->once()
-                      ->with($parameters)
-                      ->andReturn(true);
+    /**
+     * @test
+     */
+    public function publish_to_a_backend()
+    {
+        $this->flushStrategy
+             ->shouldReceive('shouldFlush')
+             ->once()
+             ->with([$this->parameters])
+             ->andReturn(true);
 
-        $publishBackend->shouldReceive('flush')
-                       ->once()
-                       ->with($parameters);
+        $this->publishBackend
+             ->shouldReceive('flush')
+             ->once()
+             ->with([$this->parameters]);
 
-        $publisher = new Publisher($flushStrategy, $publishBackend);
+        $publisher = new Publisher($this->flushStrategy, $this->publishBackend);
 
-        $publisher->publish($parameters[0]['id'], $parameters[0]['job'], $parameters[0]['event'], $parameters[0]['queue']);
-
+        $publisher->publish($this->parameters['id'], $this->parameters['job'], $this->parameters['event'], $this->parameters['queue']);
     }
 }
